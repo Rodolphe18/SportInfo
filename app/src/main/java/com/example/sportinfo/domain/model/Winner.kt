70b85c0395120065fc9e52c2440e2b5624b95f74
rfrancotte <rodolphe.francotte@webedia-group.com>
2024-12-06 14:03:10 +0100

    Winner class implemented for domain layer

--- app/src/main/java/com/example/sportinfo/domain/model/Winner.kt
+++ app/src/main/java/com/example/sportinfo/domain/model/Winner.kt
@@ -1,16 +1,22 @@
 package com.example.sportinfo.domain.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import kotlinx.serialization.Serializable
+
+@Parcelize
+@Serializable
 data class Winner(
-    val address: String,
-    val clubColors: String,
-    val crest: String,
+    val address: String?,
+    val clubColors: String?,
+    val crest: String?,
     val founded: Int,
     val winnerId: Int,
-    val lastUpdated: String,
-    val name: String,
-    val shortName: String,
-    val tla: String,
-    val venue: String,
-    val website: String
-)
+    val lastUpdated: String?,
+    val name: String?,
+    val shortName: String?,
+    val tla: String?,
+    val venue: String?,
+    val website: String?
+):Parcelable
 
